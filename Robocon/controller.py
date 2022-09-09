959cb27b7e42d28a6e53cf5811f77a0226fba646
Ea78anana <tony1201312@gmail.com>
2022-09-09 13:29:39 +0800

    123

--- Robocon/controller.py
+++ Robocon/controller.py
@@ -1,3 +1,4 @@
+from audioop import cross
 import time
 import math
 from tkinter import *
@@ -18,39 +19,41 @@ L2 = 6
 R2 = 7
 
 class controller(Node):
-    def __init__(self, name=None,triangle_publisher_topic=None, circle_publisher_topic=None):
+    def __init__(self, name=None,Right_publisher_topic=None, Left_publisher_topic=None, Reset_publisher_topic=None):
         self.name = name if name is not None else "controller"
-        self.triangle_publisher_topic = triangle_publisher_topic if triangle_publisher_topic is not None else "triangle_cmd"
-        self.circle_publisher_topic = circle_publisher_topic if circle_publisher_topic is not None else "cross_cmd"
+        self.Right_publisher_topic = Right_publisher_topic if Right_publisher_topic is not None else "Right_cmd"
+        self.Left_publisher_topic = Left_publisher_topic if Left_publisher_topic is not None else "Left_cmd"
+        self.Reset_publisher_topic = Reset_publisher_topic if Reset_publisher_topic is not None else "Reset_cmd"
+
 
         #publisher
-        self.triangle_publisher = self.create_publisher(Bool, self.triangle_publisher_topic, 10)
-        self.cross_publisher = self.create_publisher(Bool, self.cross_publisher_topic, 10)
+        self.Right_publisher = self.create_publisher(Bool, self.Right_publisher_topic, 10)
+        self.Left_publisher = self.create_publisher(Bool, self.Left_publisher_topic, 10)
+        self.Reset_publisher = self.create_publisher(Bool, self.Reset_publisher_topic, 10)
 
-        self.triangle = Bool()
-        self.triangle.data = False
-        self.cross = Bool()
-        self.cross.data = False
+
+        self.Right = Bool()
+        self.Right.data = False
+        self.Left = Bool()
+        self.Left.data = False
+        self.Reset = Bool()
+        self.Reset.data = False
+
         #subsriber
         self.subscription = self.create_subscription(Joy, "joy", self.joyCallback, 10)
 
     def joyCallback(self, msg):
-        if msg.axes[7] == 1:
-            self.Dup.data = not self.Dup.data
+        if (msg.axes[7] and msg.axes[6] == 1):
+            self.Reset.data = not self.Reset.data
             self.get_logger().info(f'Reset: {self.Reset.data}')
-            self.Dup_publisher.publish(self.Dup)
-        
-        if msg.axes[7] == -1:
-            self.Ddow.data = not self.Ddow.data
-            self.get_logger().info(f'Right: {self.Right.data}')
-            self.Ddow_publisher.publish(self.Ddow)
+            self.Reset_publisher.publish(self.Reset)
 
-        if msg.axes[6] == 1:
-            self.Dup.data = not self.Dup.data
-            self.get_logger().info(f'Reset: {self.Reset.data}')
-            self.Dup_publisher.publish(self.Dup)
+        if msg.axes[7] == -1:
+            self.Right.data = not self.Right.data
+            self.get_logger().info(f'Right: {self.Right.data}')
+            self.Right_publisher.publish(self.Right)
         
         if msg.axes[6] == -1:
-            self.Ddow.data = not self.Ddow.data
+            self.Left.data = not self.Left.data
             self.get_logger().info(f'Left: {self.Left.data}')
-            self.Ddow_publisher.publish(self.Ddow)
+            self.Left_publisher.publish(self.Left)
