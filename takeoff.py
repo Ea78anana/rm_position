5dcd2be17483aa80082796500c9e029c7028405c
Tony Chow <87663550+Ea78anana@users.noreply.github.com>
2023-03-20 17:26:01 +0800

    Add files via upload

--- takeoff.py
+++ takeoff.py
@@ -39,31 +39,26 @@ SPHINX_IP = "10.202.0.1"
 check_flag = False
 arrive = False
 call = True
-data_drone1 = 0
-data_drone2 = 0
+data = 0
 
 class TakeOff_Class:
 
     def __init__(self):
         self.DRONE_IP = os.environ.get(SPHINX_IP)
-        self.drone1 = olympe.Drone(ANAFI1_IP)
-        self.drone2 = olympe.Drone(SPHINX_IP)
-        self.roll1 = 0
-        self.pitch1 = 0
-        self.yaw1 = 0
-        self.gaz1 = 0
-        self.roll2 = 0
-        self.pitch2 = 0
-        self.yaw2 = 0
-        self.gaz2 = 0
-        self.current_drone1_pos = np.array([[0.],[0.],[0.]])
-        self.current_drone2_pos = np.array([[0.],[0.],[0.]])
+        self.drone = olympe.Drone(ANAFI2_IP)
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.gaz = 0
+        self.current_pos = np.array([[0.],[0.],[0.]])
         self.count = 0
+        self.path_x = {}
+        self.path_y = {}
 
-    def mpc_controll(self, pos, SSID):
+    def mpc_controll(self, pos):
             global check_flag   
             m = 1
-            T = 1
+            T = 0.5
             
             A = np.array([
                 [1, 0, 0, T, 0, 0],
@@ -71,9 +66,8 @@ class TakeOff_Class:
                 [0, 0, 1, 0, 0, T],
                 [0, 0, 0, 1, 0, 0],
                 [0, 0, 0, 0, 1, 0],
-                [0, 0, 0, 0, 0, 1]]
+                [0, 0, 0, 0, 0, 1]])
 
-                )
             B = np.array([
                 [0, 0, 0],
                 [0, 0, 0],
@@ -87,8 +81,14 @@ class TakeOff_Class:
             n = A.shape[0]  # number of states
             m = B.shape[1]  # number of inputs
             N = 10  # prediction horizon
-            if (self.count == 0 or self.count == 30):
-                Q = 1000 * np.eye(n)  # weight matrix Q
+            if (self.count == 0 or self.count == 36):
+                Q = np.array([ # weight matrix Q
+                [1, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 1]])  
             else :
                 Q = 1000 * np.array([
                 [1, 0, 0, 0, 0, 0],
@@ -98,7 +98,7 @@ class TakeOff_Class:
                 [0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0]])
             R = np.eye(m)  # weight matrix R
-            P = 1000 * np.array([
+            P = 100 * np.array([
                 [1, 0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0, 0],
                 [0, 0, 1, 0, 0, 0],
@@ -141,22 +141,23 @@ class TakeOff_Class:
             F = 2 * np.matmul(np.matmul(V.T, E.T), M)  # Matrix F
 
             # Input constraints
-            u_min = -14 * np.ones((m, 1))
-            u_max = 14 * np.ones((m, 1))
+            u_min = -2.8 * np.ones((m, 1))
+            u_max = 2.8 * np.ones((m, 1))
             z_min = np.matmul(np.kron(np.ones((N, 1)), np.eye(m)), u_min)
             z_max = np.matmul(np.kron(np.ones((N, 1)), np.eye(m)), u_max)
 
-            # Refrence value
-            xref1 = np.array([
-                [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
-                [0.3, 0.3, 0.6, 0.6, 0.85,0.85, 1.1, 1.1, 1.25, 1.25, 1.4, 1.4, 1.625, 1.625, 1.85, 1.85, 1.9, 1.9, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            #Refrence Value
+            '''
+            xref = np.array([
+                [0.8, 0.8, 0.8, 0.8, 0.8,  0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+                [0.3, 0.6, 0.85, 1.1, 1.25, 1.4, 1.625, 1.85, 1.9, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
             ])
-
-            xref2 = np.array([
-                [0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9],
-                [0.3, 0.3, 0.6, 0.6, 0.85,0.85, 1.1, 1.1, 1.25, 1.25, 1.4, 1.4, 1.625, 1.625, 1.85, 1.85, 1.9, 1.9, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            '''
+            xref = np.array([
+                [0.8, 0.78, 0.75, 0.69, 0.61, 0.51, 0.4, 0.27, 0.14, 0, -0.14, -0.27, -0.4, -0.51, -0.61, -0.69, -0.75, -0.78, -0.8, -0.78, -0.75, -0.69, -0.61, -0.51, -0.4, -0.27, -0.14, 0, 0.14, 0.27, 0.4, 0.51, 0.61, 0.69, 0.75, 0.78],
+                [0, 0.14, 0.27, 0.4, 0.51, 0.61, 0.69, 0.75, 0.78, 0.8, 0.78, 0.75, 0.69, 0.61, 0.51, 0.4, 0.27, 0.14, 0, -0.14, -0.27, -0.4, -0.51, -0.61, -0.69, -0.75, -0.78, -0.8, -0.78, -0.75, -0.69, -0.61, -0.51, -0.4, -0.27, -0.14],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
             ])
 
             # Initial value
@@ -169,158 +170,106 @@ class TakeOff_Class:
                 [0.]
             ])
 
-            if SSID == 1:
-                for i in range(3):
-                    x0[i] = pos[i] - xref1[i, self.count]
+            for i in range(3):
+                x0[i] = pos[i] - xref[i, self.count] 
 
-                # Define optimization variables
-                z = cp.Variable((N * m, 1))
+            # Define optimization variables
+            z = cp.Variable((N * m, 1))
 
-                # Define constraints
-                constraints = [z_min <= z, z <= z_max]
-                u_opt = []
-                u_opt_out = []
-                flag = True
+            # Define constraints
+            constraints = [z_min <= z, z <= z_max]
+            u_opt_a = []
+            u_opt = []
+            flag = True
 
-                while flag :
-                    obj = (1/2) * cp.quad_form(z, H)+ cp.matmul(cp.matmul(x0.T, F.T), z)
+            while flag :
+                obj = (1/2) * cp.quad_form(z, H)+ cp.matmul(cp.matmul(x0.T, F.T), z)
 
-                    # Define and solve the optimization problem
-                    prob = cp.Problem(cp.Minimize(obj), constraints)
-                    prob.solve(warm_start=True)
-                    u_opt = z[0:m].value
-                    x0 = A.dot(x0) + B.dot(u_opt)
-                    #print(x0)
-                    for i in range (3):
-                        u_opt[i] = int(u_opt[i] // 0.14)
-                    u_opt_out.extend(u_opt)
-
-                    
-                    if (np.linalg.norm(x0) <= 1e-2):
-                        flag = False
-                        self.roll1 = int(u_opt_out[0])
-                        self.pitch1 = int(u_opt_out[1])
-                        self.gaz1 = - int(u_opt_out[2])
-                        print(self.roll1)
-                        print(self.pitch1)
-                        print(self.gaz1)
-                    else:
-                        flag = True
-                        #print(x0)'''
-            elif SSID == 2:
-                for i in range(3):
-                    x0[i] = pos[i] - xref2[i, self.count]
-
-                # Define optimization variables
-                z = cp.Variable((N * m, 1))
-
-                # Define constraints
-                constraints = [z_min <= z, z <= z_max]
-                u_opt = []
-                u_opt_out = []
-                flag = True
-
-                while flag :
-                    obj = (1/2) * cp.quad_form(z, H)+ cp.matmul(cp.matmul(x0.T, F.T), z)
-
-                    # Define and solve the optimization problem
-                    prob = cp.Problem(cp.Minimize(obj), constraints)
-                    prob.solve(warm_start=True)
-                    u_opt = z[0:m].value
-                    x0 = A.dot(x0) + B.dot(u_opt)
-                    #print(x0)
-                    for i in range (3):
-                        u_opt[i] = int(u_opt[i] // 0.14)
-                    u_opt_out.extend(u_opt)
-
-                    
-                    if (np.linalg.norm(x0) <= 1e-2):
-                        flag = False
-                        self.roll2 = int(u_opt_out[0])
-                        self.pitch2 = int(u_opt_out[1])
-                        self.gaz2 = - int(u_opt_out[2])
-                        print(self.roll2)
-                        print(self.pitch2)
-                        print(self.gaz2)
-                    else:
-                        flag = True
-                        #print(x0)'''
+                # Define and solve the optimization problem
+                prob = cp.Problem(cp.Minimize(obj), constraints)
+                prob.solve(warm_start=True)
+                u_opt = z[0:m].value
+                x0 = A.dot(x0) + B.dot(u_opt)
+                #print(x0)
+                for i in range (3):
+                    u_opt[i] = int(u_opt[i] // 0.14)
+                u_opt_a.extend(u_opt)
+                
+                if (np.linalg.norm(x0) <= 1e-2):
+                    flag = False
+                    self.roll = int(u_opt_a[0])
+                    self.pitch = int(u_opt_a[1])
+                    self.gaz = - int(u_opt_a[2])
+                    print(self.roll)
+                    print(self.pitch)
+                    print(self.gaz)
+                else:
+                    flag = True
+                    #print(x0)'''
     
-    def position_callback(self, msg1, msg2):
+    def position_callback(self, msg):
         #print("position_callback")
         global check_flag
         if check_flag == False:
             check_flag = True
-            self.MPC(msg1, msg2)
+            self.MPC(msg)
 
     def move(self):
         pass_time = 0
         start_time = time.time()
         while pass_time < 0.5:
-            self.drone1(PCMD(1, self.roll1, self.pitch1, self.yaw1, self.gaz1, 0))
-            self.drone2(PCMD(1, self.roll2, self.pitch2, self.yaw2, self.gaz2, 0))
+            self.drone(PCMD(1, self.roll, self.pitch, self.yaw, self.gaz, 0))
             pass_time = time.time() - start_time
         print(pass_time)
-        #self.drone1(PCMD(0, 0, 0, 0, 0, 0))
+        #self.drone(PCMD(0, 0, 0, 0, 0, 0))
 
-    def MPC(self, data1, data2):
+    def MPC(self, data):
         #print("MPC")
         global arrive, call, check_flag
         
-        xref1 = np.array([[0.8],
-                        [1.85],
+        xref = np.array([[0.8],
+                        [1.95],
                         [1]])
 
-        xref2 = np.array([[-0.4],
-                        [1.85],
-                        [1]])
-
-        self.current_drone1_pos[0] = round(data1.pose.position.x,2)
-        self.current_drone1_pos[1] = round(data1.pose.position.y,2)
-        self.current_drone1_pos[2] = round(data1.pose.position.z,2)
-        self.current_drone2_pos[0] = round(data2.pose.position.x,2)
-        self.current_drone2_pos[1] = round(data2.pose.position.y,2)
-        self.current_drone2_pos[2] = round(data2.pose.position.z,2)
-        print(self.current_drone1_pos)
+        self.current_pos[0] = round(data.pose.position.x,2)
+        self.current_pos[1] = round(data.pose.position.y,2)
+        self.current_pos[2] = round(data.pose.position.z,2)
+        '''
+        self.path_x.extend(self.current_pos[0])
+        self.path_y.extend(self.current_pos[1])'''
+        print(self.current_pos)
         call = False
-        #(abs(self.current_drone1_pos[2] - xref[2]) <= 1e-1)
-        
-        if  (abs(self.current_drone1_pos[0]) - xref1[0] <= 1e-2) and (abs(self.current_drone1_pos[1]) - xref1[1] <= 1e-2) and self.count == 29:
+        #(abs(self.current_pos[2] - xref[2]) <= 1e-1)
+        '''
+        if  (abs(self.current_pos[0]) - xref[0] <= 1e-2) and (abs(self.current_pos[1]) - xref[1] <= 1e-2) and self.count == 35:
             arrive = True
             print("arrive")
             return
+        else:'''
+        #print("else")
+        self.mpc_controll(self.current_pos)
+        self.move()
+        if self.count < 35:
+            self.count += 1
         else:
-            #print("else")
-            self.mpc_controll(self.current_drone1_pos, 1)
-            self.mpc_controll(self.current_drone2_pos, 2)
-            self.move()
-            if self.count < 29:
-                self.count += 1
-            else:
-                pass
-            #print("done")
-            call = True
-            check_flag = False
+            pass
+        #print("done")
+        call = True
+        check_flag = False
 
     def disconnection(self):
-        self.drone1(Landing()).wait().success()
-        self.drone2(Landing()).wait().success()
-        self.drone1.disconnect()
-        self.drone2.disconnect()
+        self.drone(Landing()).wait().success()
+        self.drone.disconnect()
 
     def connection(self):
-        self.drone1.connect()
-        self.drone2.connect()
-        self.drone1(TakeOff()).wait().success()
-        self.drone2(TakeOff()).wait().success()
+        self.drone.connect()
+        self.drone(TakeOff()).wait().success()
 
     def listener(self):
-        global data_drone1, data_drone2
-        #sub = rospy.Subscriber('/natnet_ros/Drone1/pose', PoseStamped, self.position_callback)
-        data_drone1 = rospy.wait_for_message('/natnet_ros/Drone1/pose', PoseStamped,)
-        data_drone2 = rospy.wait_for_message('/natnet_ros/Drone2/pose', PoseStamped,)
-        #print(data)
-        #rospy.spin()
+        global data
+        #data = rospy.wait_for_message('/natnet_ros/Drone1/pose', PoseStamped,)
+        data = rospy.wait_for_message('/natnet_ros/Drone2/pose', PoseStamped,)
+
 
 if __name__ == "__main__":
     rospy.init_node('drone_takeoff_node', anonymous=False)
@@ -328,27 +277,23 @@ if __name__ == "__main__":
     takeoff = TakeOff_Class()
     takeoff.connection()
     
-            # Initial value
     time.sleep(5)
     '''
     while arrive != True:
         if call == True:
             takeoff.listener()
-            takeoff.position_callback(data_drone1, data_drone2)
+            takeoff.position_callback(data)
         time.sleep(1)
     '''   
-    for i in range(30):
+    for i in range(36):
         if call == True:
             takeoff.listener()
-            takeoff.position_callback(data_drone1, data_drone2)
-        time.sleep(1)   
-    #takeoff.move()    
-    #time.sleep(5)
-    #while check_flag == False:
+            takeoff.position_callback(data)
+        time.sleep(0.5)
+    
     takeoff.disconnection()
     print("Landed")
-    #time.sleep(2.5)
-    #takeoff.test_takeoff()
+
 
 
 '''
@@ -356,6 +301,18 @@ xref = np.array([
                 [0.97, 0.47, -0.03, -0.07, -0.07, -0.09, 0.42, 0.81,  0.78, 1.07, 1.1, 1.1],
                 [-1.55, -1.38, -1.34, -0.9, -0.42, 0.16, 0.53, 0.882, 1.53, 1.83, 2.19, 2.19],
                 [1.09, 1.2, 1.4, 1.5, 1.6, 1.8, 2, 1.8, 1.6, 1.4, 1.2, 1.09],
-            ])'''
+            ])
 
+xref = np.array([
+                [0.97, 0.47, 0.13, -0.03, -0.07, -0.07, -0.07, 0.42, 0.53, 0.78, 0.81, 1.07, 1.1, 1, 0.8, 0.5, 0.2, 0, 0, 0],
+                [-1.55, -1.38, -1.38, -1.34, -0.9, -0.42, 0.16, 0.53, 0.68, 0.882, 1.2, 1.5, 1.8, 2, 1.8, 1.6, 1.4, 1.2, 1, 0],
+                [1.09, 1.2, 1.4, 1.4, 1.5, 1.6, 1.8, 2, 1.8, 1.6, 1.4, 1.4, 1.2, 1.09, 1.09, 1.09, 1.09, 1.09, 1.09, 1.09],
+            ])
+
+xref = np.array([
+                [0.8, 0.8, 0.8, 0.8, 0.8,  0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8],
+                [0.3, 0.6, 0.85, 1.1, 1.25, 1.4, 1.625, 1.85, 1.9, 1.95, 1.95, 1.95, 1.95, 1.95, 1.95],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            ])'''
+        
 
